71707e6648cf745a03efc1cfed32f4adcf7ea609
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2015-12-03 16:44:36 +0100

    apel
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.Tep/Terradue/Tep/External/Apel/ApelServer.cs
+++ Terradue.Tep/Terradue/Tep/External/Apel/ApelServer.cs
@@ -5,6 +5,12 @@
 \defgroup Apel Apel
 @{
 
+APEL is an accounting tool that collects accounting data from sites participating in the EGI and WLCG infrastructures as well as from sites belonging to other Grid organisations that are collaborating with EGI, including OSG, NorduGrid and INFN.
+
+The accounting information is gathered from different resource providers into a central accounting database where it is processed to generate statistical summaries that are available through the reporting interface
+
+We use this system in TEP to record the usage on the different resources provider of the platform in order to provide a central accounting mechanism for the platform.
+
 @}
 
 \defgroup ApelServer Apel Server
