a9af33a9edcbef5b091c04224cbb0abfcdded8c6
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2016-01-15 12:34:56 +0100

    doc
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.Tep/Terradue/Tep/Controller/Accounting/Account.cs
+++ Terradue.Tep/Terradue/Tep/Controller/Accounting/Account.cs
@@ -13,9 +13,40 @@ Accounting to user/group accounts
 
 Periodically, it collects the usage records from the \ref ApelServer and use them to balance
 the user accounts with credits.
+The following figure describes this process.
 
+\startuml "Accounting to user/group accounts sequence diagram"
 
-<em>Here shall be a sequence diagram to describe the periodical user account balance</em>
+  control "Portal Agent" as PA
+  participant "Portal" as P
+  database "Portal database" as PDB
+  database "APEL reporting" as AR
+  
+  autonumber
+
+  loop every n sec
+
+      PA -> P : Start accounting adjustment
+      activate P
+      P <-> PDB : load last sync time
+      P <-> PDB : load users and groups accounts
+      P -> AR : request integrated accounting for users and groups
+      activate AR
+      AR -> AR : aggregate accounts
+      AR -> P : accounts
+      deactivate AR
+      loop for every user and groups in accounts
+        P <-> PDB : load rates for items in accounts
+        P -> P : control eventual deposit
+        P -> P : debit user or group with cost
+        P -> P : credit user or group provider with cost
+        P <-> PDB : save account
+      end
+      deactivate P
+
+  end
+
+\enduml
 
 
 
@@ -24,8 +55,37 @@ Credit Authorization and Quota restrictions
 
 The quota restriction is implemented using the balance of the user or group account used to query the resource.
 
+The following figure describes the example of the portal controlling the cost of a processing service and then
+check that the balance on the user account requesting it is sufficient.
 
-<em>Here shall be a sequence diagram to describe the credit authorization mechanism and the quota restriction</em>
+\startuml "Quota control using credits example on WPS sequence diagram"
+
+  actor "User" as U
+  participant "Portal" as P
+  database "Portal database" as PDB
+  participant "WPS Service" as WPS
+  
+  autonumber
+
+  U -> P : Request processing
+  activate P
+  P <-> PDB : load user account
+  P -> WPS : execute request (quotation mode)
+  activate WPS
+  WPS -> WPS : quote processing based on params
+  WPS -> P : WPS result (quotation)
+  deactivate WPS
+  P -> P : check user balance
+  alt enough credit
+    P -> WPS : execute request (normal mode)
+    P <-> PDB : update account with deposit charged
+    P -> U : request confirmation (id)
+  else not enough credit
+    P -> U : request refused
+  end
+  deactivate P
+
+\enduml
 
 
 \xrefitem int "Interfaces" "Interfaces" connects \ref ApelReporting to retrieve the aggregated usage records.
