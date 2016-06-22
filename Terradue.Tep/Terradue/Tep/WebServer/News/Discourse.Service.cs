27c0f1b5cb708245204e6a6a224d351c5b21cf60
eboissier <enguerran.boissier@terradue.com>
2016-06-22 21:22:44 +0200

    Add discourse

---
+++ Terradue.Tep/Terradue/Tep/WebServer/News/Discourse.Service.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack.ServiceHost;
+using System.Net;
+
+namespace Terradue.Tep.WebServer.Services {
+    [Api("Tep Terradue webserver")]
+    [Restrict(EndpointAttributes.InSecure | EndpointAttributes.InternalNetworkAccess | EndpointAttributes.Json,
+              EndpointAttributes.Secure | EndpointAttributes.External | EndpointAttributes.Json)]
+    public class DiscourseServiceTep : ServiceStack.ServiceInterface.Service {
+
+        public object Get(GetDiscourseTopicsPerCategory request){
+            return GetDiscourseRequest(string.Format("c/{0}.json", request.catId));
+        }
+
+        public object Get(GetDiscourseLatestTopicsPerCategory request){
+            return GetDiscourseRequest(string.Format("c/{0}/l/latest.json", request.catId));
+        }
+
+        public object Get(GetDiscourseTopic request){
+            return GetDiscourseRequest(string.Format("t/{0}.json", request.topicId));
+        }
+
+        private object GetDiscourseRequest(string query){
+            var discourseBaseUrl = "https://discuss.terradue.com";
+            var discourseUrl = string.Format("{0}/{1}", discourseBaseUrl, query);
+            HttpWebRequest httprequest = (HttpWebRequest)WebRequest.Create(discourseUrl);
+            httprequest.Method = "GET";
+            httprequest.ContentType = "application/json";
+            httprequest.Accept = "application/json";
+
+            HttpWebResponse httpResponse = (HttpWebResponse)httprequest.GetResponse();
+            return httpResponse.GetResponseStream();
+        }
+
+    }
+
+    [Route("/discourse/c/{catId}/l/latest", "GET", Summary = "", Notes = "")]
+    public class GetDiscourseLatestTopicsPerCategory {
+        [ApiMember(Name="catId", Description = "request", ParameterType = "query", DataType = "int", IsRequired = true)]
+        public int catId{ get; set; }
+    }
+
+    [Route("/discourse/c/{catId}", "GET", Summary = "", Notes = "")]
+    public class GetDiscourseTopicsPerCategory {
+        [ApiMember(Name="catId", Description = "request", ParameterType = "query", DataType = "int", IsRequired = true)]
+        public int catId{ get; set; }
+    }
+
+    [Route("/discourse/t/{topicId}", "GET", Summary = "", Notes = "")]
+    public class GetDiscourseTopic {
+        [ApiMember(Name="topicId", Description = "request", ParameterType = "query", DataType = "int", IsRequired = true)]
+        public int topicId{ get; set; }
+    }
+}
+
