03ced3e359f8835fd0149cdbb1ad80e807c54354
eboissier <enguerran.boissier@terradue.com>
2016-10-11 10:57:58 +0200

    sql

---
+++ Terradue.Tep/Resources/db/db-1.0.36#.sql
@@ -0,0 +1,6 @@
+USE $MAIN$;
+
+-- Add type
+CALL add_type(NULL, 'Terradue.Tep.ThematicApplicationSet, Terradue.Tep', 'Terradue.Tep.DataPackage, Terradue.Tep', 'Thematic Apps', 'Thematic Apps', 'apps');
+-- RESULT
+
