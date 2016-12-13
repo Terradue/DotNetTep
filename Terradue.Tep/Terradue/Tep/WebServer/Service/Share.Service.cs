f29ec88e0e158e530c00711e6376c99d1982eee1
eboissier <enguerran.boissier@terradue.com>
2016-12-13 14:36:57 +0100

    up share

--- Terradue.Tep/Terradue/Tep/WebServer/Service/Share.Service.cs
+++ Terradue.Tep/Terradue/Tep/WebServer/Service/Share.Service.cs
@@ -116,10 +116,15 @@ namespace Terradue.Tep.WebServer.Services {
                 } else if (resultType.Equals(EntityType.GetEntityType(typeof(WpsProcessOffering)).Keyword)) {
                     redirectUrl += "resultType=" + EntityType.GetEntityType(typeof(WpsProcessOffering)).Keyword;
                 } else {
-                    if (request.url.StartsWith(context.GetConfigValue("catalog-baseurl")) || request.url.StartsWith ("https://data.terradue.com") || request.url.StartsWith("https://data2.terradue.com")) {
+                    if (request.url.StartsWith(context.GetConfigValue("catalog-baseurl"))) {
                         redirectUrl += "resultType=" + "data";
                     } else {
-                        redirectUrl += "resultType=" + "na";
+                        try {
+                            var os = new GenericOpenSearchable (new OpenSearchUrl (request.url), MasterCatalogue.OpenSearchEngine);
+                            redirectUrl += "resultType=" + "data";
+                        } catch (Exception e) { 
+                            redirectUrl += "resultType=" + "na";
+                        }
                     }
                 }
                 redirectUrl += "&url=" + HttpUtility.UrlEncode(request.url);
