cb1c09a06a09bda571f035a298ada9ed6310b518
eboissier <enguerran.boissier@terradue.com>
2017-02-02 15:52:24 +0100

    db

--- Terradue.Tep/Resources/db/db-1.1.2#.sql
+++ Terradue.Tep/Resources/db/db-1.1.2#.sql
@@ -5,5 +5,6 @@ USE $MAIN$;
 
 -- Update roles ... \
 INSERT INTO role (identifier, name, description) VALUES ('member', 'member', 'Community default member');
+INSERT INTO role (identifier, name, description) VALUES ('manager', 'manager', 'Community manager');
 -- RESULT
 
