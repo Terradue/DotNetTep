37f0da88ec2e056c4f23d2e02852d8da29eb9eed
eboissier <enguerran.boissier@terradue.com>
2017-02-03 17:21:40 +0100

    up tests

--- Terradue.Tep.Test/Wpsjob.Test.cs
+++ Terradue.Tep.Test/Wpsjob.Test.cs
@@ -13,17 +13,54 @@ namespace Terradue.Tep.Test {
             base.FixtureSetup();
             context.BaseUrl = "http://localhost:8080/api";
             context.AccessLevel = EntityAccessLevel.Administrator;
-            CreateUsers();
+            Init ();
+
+            context.ConsoleDebug = true;
+            EntityType et = EntityType.GetOrAddEntityType (typeof (WpsJob));
+            Console.WriteLine ("CLASS {0} {1}", et.ClassType.AssemblyQualifiedName, et.Id);
         }
 
-        private void CreateUsers() {
-            User usr1 = new User(context);
+        private void Init() {
+
+            //Create users
+            UserTep usr1 = new UserTep(context);
             usr1.Username = "testusr1";
             usr1.Store();
+            usr1.CreatePrivateDomain ();
 
-            User usr2 = new User(context);
+            UserTep usr2 = new UserTep(context);
             usr2.Username = "testusr2";
             usr2.Store();
+            usr2.CreatePrivateDomain ();
+
+            UserTep usr3 = new UserTep (context);
+            usr3.Username = "testusr3";
+            usr3.Store ();
+            usr3.CreatePrivateDomain ();
+
+            //create domains
+            Domain domain = new Domain (context);
+            domain.Name = "myDomainTest";
+            domain.Kind = DomainKind.Public;
+            domain.Store ();
+
+            Domain domain2 = new Domain (context);
+            domain2.Name = "otherDomainTest";
+            domain2.Kind = DomainKind.Private;
+            domain2.Store ();
+
+            Role role = new Role (context);
+            role.Identifier = "member";
+            role.Store ();
+
+            role.IncludePrivilege (Privilege.FromIdentifier (context, "wpsjob-v"));
+            role.IncludePrivilege (Privilege.FromIdentifier (context, "wpsjob-s"));
+
+            //Add users in the domain
+            role.GrantToUser (usr1, domain);
+            role.GrantToUser (usr2, domain);
+            role.GrantToUser (usr3, domain);
+            role.GrantToUser (usr3, domain2);
         }
 
         private WpsProvider CreateProvider(string identifier, string name, string url, bool proxy) {
@@ -60,54 +97,95 @@ namespace Terradue.Tep.Test {
             return process;
         }
 
-        private WpsJob CreateWpsJob(string name, WpsProcessOffering wps, int OwnerId) {
+        private WpsJob CreateWpsJob(string name, WpsProcessOffering wps, User owner) {
             WpsJob wpsjob = new WpsJob(context);
             wpsjob.Name = name;
             wpsjob.RemoteIdentifier = Guid.NewGuid().ToString();
             wpsjob.Identifier = Guid.NewGuid().ToString();
-            wpsjob.OwnerId = OwnerId;
-            wpsjob.UserId = OwnerId;
+            wpsjob.OwnerId = owner.Id;
+            wpsjob.UserId = owner.Id;
             wpsjob.WpsId = wps.Provider.Identifier;
             wpsjob.ProcessId = wps.Identifier;
             wpsjob.CreatedTime = DateTime.UtcNow;
+            wpsjob.DomainId = owner.DomainId;
             wpsjob.Parameters = new List<KeyValuePair<string, string>>();
             wpsjob.StatusLocation = "http://dem.terradue.int:8080/wps/WebProcessingService";
             return wpsjob;
         }
 
         [Test]
-        public void LoadWpsJob() {
-            WpsProcessOffering process = CreateProcess(false);
+        public void CreateWpsJobs () { 
+            
+            WpsProcessOffering process = CreateProcess (false);
+            var usr1 = User.FromUsername (context, "testusr1");
+            var usr2 = User.FromUsername (context, "testusr2");
+            var usr3 = User.FromUsername (context, "testusr3");
+            var domain = Domain.FromIdentifier (context, "myDomainTest");
+            var domain2 = Domain.FromIdentifier (context, "otherDomainTest");
 
-            var usr1 = User.FromUsername(context, "testusr1");
-            var usr2 = User.FromUsername(context, "testusr2");
+            //Create one wpsjob public for usr1 --- all should see it
+            WpsJob job = CreateWpsJob ("public-job-usr1", process, usr1);
+            job.Store ();
+            job.GrantGlobalPermissions ();
 
-            context.ConsoleDebug = true;
+            //Create one wpsjob with domain where usr1 is member
+            job = CreateWpsJob ("public-job-d", process, usr2);
+            job.Domain = domain;
+            job.Store ();
 
-            //Create one wpsjob public
-            WpsJob job1 = CreateWpsJob("private-job", process, usr1.Id);
-            job1.Store();
-            job1.GrantGlobalPermissions();
+            //Create one wpsjob with domain where usr1 is not member
+            job = CreateWpsJob ("public-job-d", process, usr3);
+            job.Domain = domain2;
+            job.Store ();
 
-            //Create one wpsjob restricted
-            WpsJob job2 = CreateWpsJob("restricted-job", process, usr1.Id);
-            job2.Store();
-            job2.GrantPermissionsToUsers(new int [] { usr2.Id });
+            //Create one wpsjob public for usr2 --- all should see it
+            job = CreateWpsJob ("public-job-usr2", process, usr2);
+            job.Store ();
+            job.GrantGlobalPermissions ();
 
-            //Create one wpsjob private
-            WpsJob job3 = CreateWpsJob("public-job", process, usr1.Id);
-            job3.Store();
+            //Create one wpsjob restricted for usr1
+            job = CreateWpsJob ("restricted-job-usr1-2", process, usr1);
+            job.Store ();
+            job.GrantPermissionsToUsers (new int [] { usr2.Id });
 
-            //Create one wpsjob private
-            WpsJob job4 = CreateWpsJob("other-job", process, usr2.Id);
-            job4.Store();
+            //Create one wpsjob restricted for usr2
+            job = CreateWpsJob ("restricted-job-usr2-1", process, usr2);
+            job.Store ();
+            job.GrantPermissionsToUsers (new int [] { usr1.Id });
 
-            EntityType et = EntityType.GetOrAddEntityType(typeof(WpsJob));
-            Console.WriteLine("CLASS {0} {1}", et.ClassType.AssemblyQualifiedName, et.Id);
+            //Create one wpsjob restricted for usr2
+            job = CreateWpsJob ("restricted-job-usr2-3", process, usr2);
+            job.Store ();
+            job.GrantPermissionsToUsers (new int [] { usr3.Id });
 
+            //Create one wpsjob private for usr1
+            job = CreateWpsJob ("private-job-usr1", process, usr1);
+            job.Store ();
+
+            //Create one wpsjob private for usr2
+            job = CreateWpsJob ("private-job-usr2", process, usr2);
+            job.Store ();
+
+            //Create one wpsjob private for usr3
+            job = CreateWpsJob ("private-job-usr3", process, usr3);
+            job.Store ();
+
+            context.AccessLevel = EntityAccessLevel.Administrator;
+            EntityList<WpsJob> jobList = new EntityList<WpsJob> (context);
+            jobList.Load ();
+            var items = jobList.GetItemsAsList ();
+            Assert.AreEqual (10, items.Count);
+        }
+
+        [Test]
+        public void LoadWpsJobByVisibility() {
+            
+            context.AccessLevel = EntityAccessLevel.Privilege;            
+
+            var usr1 = User.FromUsername (context, "testusr1");
             context.StartImpersonation(usr1.Id);
 
-            //Test Visibility owned
+            //Test Visibility OWNED
             EntityList<WpsJob> jobList = new EntityList<WpsJob>(context);
             jobList.AccessLevel = EntityAccessLevel.Privilege;
             jobList.ItemVisibility = EntityItemVisibility.OwnedOnly;
@@ -115,31 +193,100 @@ namespace Terradue.Tep.Test {
             var items = jobList.GetItemsAsList();
             Assert.AreEqual(3, items.Count);
 
-            //Test Visibility public
+            //Test Visibility PUBLIC
             jobList = new EntityList<WpsJob>(context);
             jobList.ItemVisibility = EntityItemVisibility.Public;
             jobList.Load();
             items = jobList.GetItemsAsList();
-            Assert.AreEqual(1, items.Count);
-            Assert.That(items[0].Name == "public-job");
+            Assert.AreEqual(2, items.Count);
+            foreach (var item in items) Assert.That (item.Name.Contains ("public"));
 
-            //Test Visibility restricted
-            jobList = new EntityList<WpsJob>(context);
-            jobList.ItemVisibility = EntityItemVisibility.Public;
-            jobList.Load();
-            items = jobList.GetItemsAsList();
-            Assert.AreEqual(1, items.Count);
-            Assert.That(items[0].Name == "restricted-job");
+            //Test Visibility PUBLIC | OWNED 
+            jobList = new EntityList<WpsJob> (context);
+            jobList.ItemVisibility = EntityItemVisibility.Public | EntityItemVisibility.OwnedOnly;
+            jobList.Load ();
+            items = jobList.GetItemsAsList ();
+            Assert.AreEqual (1, items.Count);
+            Assert.That (items[0].Name == "public-job-usr1");
 
-            //Test Visibility private
+            //Test Visibility RESTRICTED
             jobList = new EntityList<WpsJob>(context);
-            jobList.ItemVisibility = EntityItemVisibility.Public;
+            jobList.ItemVisibility = EntityItemVisibility.Restricted;
+            jobList.Load();
+            items = jobList.GetItemsAsList();
+            Assert.AreEqual (2, items.Count);
+            foreach (var item in items) Assert.That (item.Name.Contains ("restricted"));
+
+            //Test Visibility RESTRICTED | OWNED
+            jobList = new EntityList<WpsJob> (context);
+            jobList.ItemVisibility = EntityItemVisibility.Restricted | EntityItemVisibility.OwnedOnly;
+            jobList.Load ();
+            items = jobList.GetItemsAsList ();
+            Assert.AreEqual (1, items.Count);
+            Assert.That (items [0].Name == "restricted-job-usr1");
+
+            //Test Visibility PRIVATE
+            jobList = new EntityList<WpsJob>(context);
+            jobList.ItemVisibility = EntityItemVisibility.Private;
             jobList.Load();
             items = jobList.GetItemsAsList();
             Assert.AreEqual(1, items.Count);
-            Assert.That(items[0].Name == "private-job");
+            Assert.That(items[0].Name == "private-job-usr1");
+
+            //Test Visibility PRIVATE | OWNED
+            jobList = new EntityList<WpsJob> (context);
+            jobList.ItemVisibility = EntityItemVisibility.Private | EntityItemVisibility.OwnedOnly;
+            jobList.Load ();
+            items = jobList.GetItemsAsList ();
+            Assert.AreEqual (1, items.Count);
+            Assert.That (items [0].Name == "private-job-usr1");
 
             context.EndImpersonation();
+        }
+
+        [Test]
+        public void LoadWpsJobByDomain (){ 
+            context.AccessLevel = EntityAccessLevel.Privilege;
+
+            var usr1 = User.FromUsername (context, "testusr1");
+            context.StartImpersonation (usr1.Id);
+            var domain = Domain.FromIdentifier (context, "myDomainTest");
+
+            EntityList<WpsJob> jobList = new EntityList<WpsJob> (context);
+            jobList.DomainId = domain.Id;
+            jobList.Load ();
+            var items = jobList.GetItemsAsList ();
+            Assert.AreEqual (1, items.Count);
+            Assert.That (items [0].Name == "public-job-d");
+        }
+
+        [Test]
+        public void LoadWpsJobByOwner ()
+        {
+            context.AccessLevel = EntityAccessLevel.Privilege;
+
+            var usr1 = User.FromUsername (context, "testusr1");
+            var usr2 = User.FromUsername (context, "testusr2");
+            var usr3 = User.FromUsername (context, "testusr3");
+            context.StartImpersonation (usr1.Id);
+
+            EntityList<WpsJob> jobList = new EntityList<WpsJob> (context);
+            jobList.UserId = usr1.Id;
+            jobList.Load ();
+            var items = jobList.GetItemsAsList ();
+            Assert.AreEqual (3, items.Count);
+
+            jobList = new EntityList<WpsJob> (context);
+            jobList.UserId = usr2.Id;
+            jobList.Load ();
+            items = jobList.GetItemsAsList ();
+            Assert.AreEqual (2, items.Count);
+
+            jobList = new EntityList<WpsJob> (context);
+            jobList.UserId = usr3.Id;
+            jobList.Load ();
+            items = jobList.GetItemsAsList ();
+            Assert.AreEqual (0, items.Count);
         }
 
     }
