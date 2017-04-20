c3325904073bbb141848b130c4b16492f65df21b
eboissier <enguerran.boissier@terradue.com>
2017-04-20 18:17:13 +0200

    wps auto update

--- Terradue.Tep/Resources/db/db-1.1.2.5#.sql
+++ Terradue.Tep/Resources/db/db-1.1.2.5#.sql
@@ -4,5 +4,5 @@ USE $MAIN$;
 /*****************************************************************************/
 
 -- Adding Agent action...\
-INSERT INTO action (`identifier`, `name`, `description`, `class`, `method`) VALUES ('wpsSynchro', 'Synchronize WPS', 'This action synchronize the wps providers stored in db', 'Terradue.Tep.Actions, Terradue.Tep', 'UpdateWpsProviders');
+INSERT INTO action (`identifier`, `name`, `description`, `class`, `method`, `enabled`) VALUES ('wpsSynchro', 'Synchronize WPS', 'This action synchronize the wps providers stored in db', 'Terradue.Tep.Actions, Terradue.Tep', 'UpdateWpsProviders',1);
 -- RESULT
