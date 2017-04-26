fea5658d5db8b3e6016bc1f479d88aa948ee16b7
eboissier <enguerran.boissier@terradue.com>
2017-04-26 15:44:27 +0200

    log

--- Terradue.Tep/Terradue/Tep/WebServer/Agent/Actions.cs
+++ Terradue.Tep/Terradue/Tep/WebServer/Agent/Actions.cs
@@ -9,12 +9,16 @@ namespace Terradue.Tep {
         /// </summary>
         /// <param name="context">Context.</param>
         public static void UpdateWpsProviders(IfyContext context) {
-            var wpsProviders = new EntityList<WpsProvider>(context);
-            wpsProviders.Load();
+            try {
+                var wpsProviders = new EntityList<WpsProvider>(context);
+                wpsProviders.Load();
 
-            foreach(var provider in wpsProviders.GetItemsAsList()){
-                context.WriteInfo(string.Format("AGENT -- WPS {0} - Auto synchro = {1}",provider.Name, provider.AutoSync ? "true" : "false"));
-                if (provider.AutoSync) provider.UpdateProcessOfferings(true);
+                foreach (var provider in wpsProviders.GetItemsAsList()) {
+                    context.WriteInfo(string.Format("UpdateWpsProviders -- WPS {0} - Auto synchro = {1}", provider.Name, provider.AutoSync ? "true" : "false"));
+                    if (provider.AutoSync) provider.UpdateProcessOfferings(true);
+                }
+            } catch (Exception e) {
+                context.WriteError(string.Format("UpdateWpsProviders -- {0} - {1}", e.Message, e.StackTrace));
             }
         }
     }
