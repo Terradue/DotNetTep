8ab4595703ca0d0c768911527ac3892ad8bf7a73
eboissier <enguerran.boissier@terradue.com>
2017-05-03 09:49:20 +0200

    déconfit

--- Terradue.Tep/Terradue/Tep/WebServer/Config/Config.Service.cs
+++ Terradue.Tep/Terradue/Tep/WebServer/Config/Config.Service.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using ServiceStack.ServiceHost;
 using ServiceStack.ServiceInterface;
 using Terradue.OpenNebula;
@@ -31,5 +32,90 @@ namespace Terradue.Tep.WebServer.Services {
             }
             return result;
         }
-    }
+
+        public object Get(GetDBConfigRequestTep request) {
+            List<WebDBConfig> result = new List<WebDBConfig>();
+
+            var context = TepWebContext.GetWebContext(PagePrivileges.AdminOnly);
+            try {
+                context.Open();
+
+                string sql = "SELECT name,value,type FROM config;";
+                IDbConnection dbConnection = context.GetDbConnection();
+                IDataReader reader = context.GetQueryResult(sql, dbConnection);
+
+                while (reader.Read()) {
+                    try {
+                        result.Add(new WebDBConfig {
+                            Name = reader.GetString(0),
+                            Value = (reader.GetValue(1) == System.DBNull.Value ? "" : reader.GetString(1)),
+                            Type = reader.GetString(2)
+                        });
+                    } catch (Exception e) {
+                        context.LogError(this, e.Message);
+                    }
+                }
+                context.CloseQueryResult(reader, dbConnection);
+                context.Close();
+            } catch (Exception e) {
+                context.Close();
+                throw e;
+            }
+            return result;
+        }
+
+        public object Post(CreateDBConfigRequestTep request) { 
+            var context = TepWebContext.GetWebContext(PagePrivileges.AdminOnly);
+            try {
+                context.Open();
+
+                if (string.IsNullOrEmpty(request.Name)) throw new Exception("Invalid config name");
+
+                string sql = string.Format("INSERT INTO config (name, value, type) VALUES ('{0}','{1}','{2}');", request.Name, request.Value, request.Type);
+                context.Execute(sql);
+
+                context.Close();
+            } catch (Exception e) {
+                context.Close();
+                throw e;
+            }
+            return new WebResponseBool(true);
+        }
+
+        public object Put(UpdateDBConfigRequestTep request) { 
+            var context = TepWebContext.GetWebContext(PagePrivileges.AdminOnly);
+            try {
+                context.Open();
+
+                if (string.IsNullOrEmpty(request.Name)) throw new Exception("Invalid config name");
+
+                string sql = string.Format("UPDATE config SET value='{0}', type='{1}' WHERE name='{2}';", request.Value, request.Type, request.Name);
+                context.Execute(sql);
+
+                context.Close();
+            } catch (Exception e) {
+                context.Close();
+                throw e;
+            }
+            return new WebResponseBool(true);
+        }
+
+        public object Delete(DeleteDBConfigRequestTep request) {
+            var context = TepWebContext.GetWebContext(PagePrivileges.AdminOnly);
+            try {
+                context.Open();
+
+                if (string.IsNullOrEmpty(request.Name)) throw new Exception("Invalid config name");
+
+                string sql = string.Format("DELETE FROM config WHERE name='{0}';", request.Name);
+                context.Execute(sql);
+
+                context.Close();
+            } catch (Exception e) {
+                context.Close();
+                throw e;
+            }
+            return new WebResponseBool(true);
+        }
+     }
 }
---
+++ Terradue.Tep/Terradue/Tep/WebServer/Config/WebDBConfig.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using ServiceStack.ServiceHost;
+using Terradue.Portal;
+
+namespace Terradue.Tep {
+
+    [Route("/dbconfig", "GET", Summary = "GET list of all db configs", Notes = "")]
+    public class GetDBConfigRequestTep : IReturn<List<WebDBConfig>> {}
+
+    [Route("/dbconfig", "POST", Summary = "POST db config", Notes = "")]
+    public class CreateDBConfigRequestTep : WebDBConfig, IReturn<List<WebDBConfig>> {}
+
+    [Route("/dbconfig", "PUT", Summary = "POST db config", Notes = "")]
+    public class UpdateDBConfigRequestTep : WebDBConfig, IReturn<List<WebDBConfig>> { }
+
+    [Route("/dbconfig", "DELETE", Summary = "DELETE db config", Notes = "")]
+    public class DeleteDBConfigRequestTep : WebDBConfig, IReturn<List<WebDBConfig>> { }
+
+    //-------------------------------------------------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------------------------------------------------
+
+    /// <summary>
+    /// User.
+    /// </summary>
+    public class WebDBConfig {
+
+        [ApiMember(Name = "name", Description = "config name", ParameterType = "query", DataType = "string", IsRequired = false)]
+        public string Name { get; set; }
+
+        [ApiMember(Name = "value", Description = "config value", ParameterType = "query", DataType = "string", IsRequired = false)]
+        public string Value { get; set; }
+
+        [ApiMember(Name = "type", Description = "config type", ParameterType = "query", DataType = "string", IsRequired = false)]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Terradue.Tep.WebServer.WebUserTep"/> class.
+        /// </summary>
+        public WebDBConfig() { }
+
+    }
+}
