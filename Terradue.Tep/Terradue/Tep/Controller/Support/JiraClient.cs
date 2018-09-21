674d15dcc5c917081acc05d6d3cd21b3184ddf6c
eboissier <enguerran.boissier@terradue.com>
2018-09-21 16:36:40 +0200

    add jira files

---
+++ Terradue.Tep/Terradue/Tep/Controller/Support/JiraClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using ServiceStack.Text;
+
+namespace Terradue.Tep.Controller {
+    public class JiraClient {
+
+        public string APIBaseUrl { get; set; }
+        public string APIUsername { get; set; }
+        public string APIPassword { get; set; }
+
+        public JiraClient() {
+        }
+
+        public void CreateIssue(){
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(this.APIBaseUrl + "/rest/api/2/issue/createmeta");
+            request.Proxy = null;
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+            request.Credentials = new NetworkCredential(this.APIUsername, this.APIPassword);
+
+            string projectID = "TEST";
+            string summary = "This is a test";
+            string description = "This is a test description";
+            string issueType = "Bug";
+
+            var issue = new JiraIssueRequest { 
+                fields = new JiraIssueFields {
+                    project = new JiraIssueProject{ key = projectID },
+                    summary = summary,
+                    description = description,
+                    issuetype = new JiraIssuetype{
+                        name = issueType
+                    }
+                }
+            };
+
+            string json = JsonSerializer.SerializeToString<JiraIssueRequest>(issue);
+
+            using (var streamWriter = new StreamWriter(request.GetRequestStream())) {
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+
+                using (var httpResponse = (HttpWebResponse)request.GetResponse()) {
+                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream())) {
+                        string result = streamReader.ReadToEnd();
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Terradue.Tep/Terradue/Tep/Controller/Support/JiraIssue.Request.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Terradue.Tep.Controller {
+
+    [DataContract]
+    public class JiraIssueProject {
+        [DataMember]
+        public string key { get; set; }
+    }
+
+    [DataContract]
+    public class JiraIssuetype {
+        [DataMember]
+        public string name { get; set; }
+    }
+
+    [DataContract]
+    public class JiraIssueFields {
+        [DataMember]
+        public JiraIssueProject project { get; set; }
+        [DataMember]
+        public string summary { get; set; }
+        [DataMember]
+        public string description { get; set; }
+        [DataMember]
+        public JiraIssuetype issuetype { get; set; }
+    }
+
+    [DataContract]
+    public class JiraIssueRequest {
+        [DataMember]
+        public JiraIssueFields fields { get; set; }
+    }
+
+}
