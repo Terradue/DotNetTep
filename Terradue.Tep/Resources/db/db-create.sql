0916cdbe2693be354e6fa6c78ba7048af2c6b493
eboissier <enguerran.boissier@terradue.com>
2019-10-21 08:51:21 +0200

    this refs #ESAGEPSW-609 -- add request for user who wants to join

--- Terradue.Tep/Resources/db/db-create.sql
+++ Terradue.Tep/Resources/db/db-create.sql
@@ -1,4 +1,4 @@
--- VERSION 1.2.12
+-- VERSION 1.2.13
 
 USE $MAIN$;
 
@@ -421,4 +421,7 @@ INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALU
 UPDATE type SET custom_class='Terradue.Tep.WpsProcessOfferingTep, Terradue.Tep' WHERE class='Terradue.Portal.WpsProcessOffering, Terradue.Portal';
 -- RESULT
 
+-- Update config...\
+UPDATE config SET value='Dear Content Authority,\n\nthe user $(USERNAME) ($(USERMAIL)) has requested to Join the community $(COMMUNITY), with the following description:\n\n\t$(USER_REQUEST)\n\nYou can approve or refuse this request here $(LINK).\n\nYou can request information about the requesting user project to the Platform Operator.\n\nBest Regards' WHERE name='CommunityJoinEmailBody';
+-- RESULT
 
