ec15e74d6dedee4c70864c21c51b048dc1d3d921
eboissier <enguerran.boissier@terradue.com>
2021-03-04 19:00:49 +0100

    add logs + change json to atom in input for ades

--- Terradue.Tep/Terradue/Tep/WebServer/User/User.Service.cs
+++ Terradue.Tep/Terradue/Tep/WebServer/User/User.Service.cs
@@ -443,7 +443,6 @@ namespace Terradue.Tep.WebServer.Services {
             var context = TepWebContext.GetWebContext(PagePrivileges.UserView);
             try {
                 context.Open();
-                context.LogInfo(this,string.Format("/user/current/logstatus GET"));
 				UserTep.UpdateUserSessionEndTime(context, context.UserId);
             } catch (Exception e) {
                 return new WebResponseBool(false);
