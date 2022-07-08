68a58a110112880c5525a3c24a48af7bcfc159c2
eboissier <enguerran.boissier@terradue.com>
2022-07-08 09:07:15 +0200

    up images

--- Terradue.Tep/Terradue/Tep/WebServer/Features/Image.Service.cs
+++ Terradue.Tep/Terradue/Tep/WebServer/Features/Image.Service.cs
@@ -1,45 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
+using System.Linq;
+using System.Web;
 using ServiceStack.ServiceHost;
 using Terradue.Portal;
 using Terradue.Tep.WebServer;
 using Terradue.WebService.Model;
 
 namespace Terradue.Tep.WebServer.Services {
+
+    [Route("/image", "GET", Summary = "GET a list of images", Notes = "")]
+    public class GetImages : IReturn<List<string>>{
+        [ApiMember(Name="q", Description = "q", ParameterType = "query", DataType = "string", IsRequired = true)]
+        public string q { get; set; }
+    }
+
+    [Route("/image", "POST", Summary = "POST Image file")]
+    public class UploadImage : IRequiresRequestStream, IReturn<WebResponseBool>
+    {
+        public System.IO.Stream RequestStream { get; set; }
+
+        [ApiMember(Name="overwrite", Description = "overwrite", ParameterType = "query", DataType = "bool", IsRequired = true)]
+        public bool overwrite { get; set; }
+    }
+
+    [Route("/image", "DELETE", Summary = "DELETE Image file")]
+    public class DeleteImage : IReturn<WebResponseBool>{
+        [ApiMember(Name="filename", Description = "filename", ParameterType = "query", DataType = "string", IsRequired = true)]
+        public string filename { get; set; }
+    }
+
+
     [Api("Tep Terradue webserver")]
     [Restrict(EndpointAttributes.InSecure | EndpointAttributes.InternalNetworkAccess | EndpointAttributes.Json,
               EndpointAttributes.Secure | EndpointAttributes.External | EndpointAttributes.Json)]
     public class ImageServiceTep : ServiceStack.ServiceInterface.Service {
 
         public object Get(GetImages request) {
-            List<WebImage> result = new List<WebImage>();
-
-            var context = TepWebContext.GetWebContext(PagePrivileges.EverybodyView);
-            try {
-                context.Open();
-                EntityList<Image> img = new EntityList<Image>(context);
-
-                img.Load();
-                foreach(Image f in img) result.Add(new WebImage(f));
-
-                context.Close();
-            } catch (Exception e) {
-                context.Close();
-                throw e;
-            }
-            return result;
-        }
-
-        public object Get(GetImage request) {
-            WebImage result = null;
+            List<string> result = new List<string>();
 
             var context = TepWebContext.GetWebContext(PagePrivileges.EverybodyView);
             try {
                 context.Open();
 
-                Image img = Image.FromId(context,request.Id);
-                result = new WebImage(img);
+                var imgpath = context.GetConfigValue ("path.img");                
+                var rootpath = imgpath.Substring(imgpath.LastIndexOf("/root") + 5);                                
+                result = System.IO.Directory.GetFiles(imgpath, !string.IsNullOrEmpty(request.q) ? "*" + request.q + "*" : "*").ToList();
+                result = result.ConvertAll(f => rootpath + "/" + f.Substring(f.LastIndexOf("/") + 1));
 
                 context.Close();
             } catch (Exception e) {
@@ -47,99 +56,28 @@ namespace Terradue.Tep.WebServer.Services {
                 throw e;
             }
             return result;
-        }
-
-        public object Post(CreateImage request) {
-            WebImage result = null;
-
-            var context = TepWebContext.GetWebContext(PagePrivileges.AdminOnly);
-            try {
-                context.Open();
-
-                Image img = new Image(context);
-                img = request.ToEntity(context, img);
-                img.Store();
-                result = new WebImage(img);
-
-                context.Close();
-            } catch (Exception e) {
-                context.Close();
-                throw e;
-            }
-            return result;
-        }
-
-        public object Put(UpdateImage request) {
-            WebImage result = null;
-
-            var context = TepWebContext.GetWebContext(PagePrivileges.AdminOnly);
-            try {
-                context.Open();
-
-                Image img = Image.FromId(context, request.Id);
-                img = request.ToEntity(context, img);
-                img.Store();
-                result = new WebImage(img);
-
-                context.Close();
-            } catch (Exception e) {
-                context.Close();
-                throw e;
-            }
-            return result;
-        }
-
-        public object Delete(DeleteImage request) {
-            var context = TepWebContext.GetWebContext(PagePrivileges.AdminOnly);
-            try {
-                context.Open();
-                Image img = Image.FromId(context,request.Id);
-                img.Delete();
-
-                context.Close();
-            } catch (Exception e) {
-                context.Close();
-                throw e;
-            }
-            return new WebResponseBool(true);
         }
 
         public object Post(UploadImage request){
-            var context = TepWebContext.GetWebContext(PagePrivileges.UserView);
-            string img = "";
+            var context = TepWebContext.GetWebContext(PagePrivileges.AdminOnly);            
             try {
                 context.Open();
                 string uid = Guid.NewGuid().ToString();
 
-                if (request.Id == 0)
-                {
-                    var segments = base.Request.PathInfo.Split(new[] { '/' }, 
-                                                               StringSplitOptions.RemoveEmptyEntries);
-                    request.Id = System.Int32.Parse(segments[1]);
-                }
+                if (this.RequestContext.Files.Length == 0) throw new Exception("File not sent");
+                var uploadedFile = this.RequestContext.Files[0];                    
+                var filename = uploadedFile.FileName;
 
-                Image image = Image.FromId(context, request.Id);
-                string oldImg = image.Url;
-                if (this.RequestContext.Files.Length > 0)
-                {
-                    var uploadedFile = this.RequestContext.Files[0];
-                    img = uid + uploadedFile.FileName.Substring(uploadedFile.FileName.LastIndexOf("."));
-                    uploadedFile.SaveTo("files/" + img);
-                }
-                else{
-                    using (var fileStream = File.Create("files/"+uid+".png"))
-                    {
-                        img = uid + ".png";
-                        request.RequestStream.CopyTo(fileStream);
-                    }
-                }
-                image.Url = img;
-                image.Store();
-
-                try{
-                    if(oldImg != null) File.Delete("files/"+oldImg);
-                }catch(Exception){}
-
+                //check does not already exists
+                // if (System.IO.Directory.GetFiles(imgpath) != null && request.overwrite == false)
+                    // throw new Exception("File already exists");
+                
+                // try{
+                //     if(oldImg != null) File.Delete(imgpath);
+                // }catch(Exception){}
+                
+                uploadedFile.SaveTo(context.GetConfigValue ("path.img") + "/" + filename);
+                
                 context.Close();
             }catch(Exception e)
             {
@@ -148,6 +86,24 @@ namespace Terradue.Tep.WebServer.Services {
             }
             return new WebResponseBool(true);
         }
+
+        public object Delete(DeleteImage request) {
+            var context = TepWebContext.GetWebContext(PagePrivileges.AdminOnly);
+            try {
+                context.Open();
+                try{
+                    var imgpath = context.GetConfigValue ("path.img").TrimEnd('/') + "/" + request.filename;
+                    File.Delete(imgpath);
+                }catch(Exception){}
+
+                context.Close();
+            } catch (Exception e) {
+                context.Close();
+                throw e;
+            }
+            return new WebResponseBool(true);
+        }
+
     }
 }
 
