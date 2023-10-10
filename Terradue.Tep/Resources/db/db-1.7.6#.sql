a7b699e62cc87fad03f9b6c3dd26a0b78d199149
eboissier <enguerran.boissier@terradue.com>
2023-10-10 15:50:37 +0200

    up

--- Terradue.Tep/Resources/db/db-1.7.6#.sql
+++ Terradue.Tep/Resources/db/db-1.7.6#.sql
@@ -3,7 +3,7 @@ USE $MAIN$;
 /*****************************************************************************/
 
 -- Add keycloak auth...\
-INSERT INTO auth (`identifier`, `name`, `description`, `type`, `enabled`, `activation_rule`, `normal_rule`, `refresh_period`) VALUES ('keycloak', 'keycloak authentication', 'keycloak authentication', 'Terradue.Tep.Controller.Auth.KeycloakAuthenticationType, Terradue.Tep.WebServer', '0', '2', '2', '0');
+INSERT INTO auth (`identifier`, `name`, `description`, `type`, `enabled`, `activation_rule`, `normal_rule`, `refresh_period`) VALUES ('keycloak', 'keycloak authentication', 'keycloak authentication', 'Terradue.Tep.Controller.Auth.KeycloakAuthenticationType, Terradue.Tep', '0', '2', '2', '0');
 -- RESULT
 
 
