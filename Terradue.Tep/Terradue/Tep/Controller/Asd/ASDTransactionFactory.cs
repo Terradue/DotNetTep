eb84030a5118fd35be41123cd5c4d538bfec8e29
eboissier <enguerran.boissier@terradue.com>
2024-01-25 16:01:11 +0100

    up negative balance

--- Terradue.Tep/Terradue/Tep/Controller/Asd/ASDTransactionFactory.cs
+++ Terradue.Tep/Terradue/Tep/Controller/Asd/ASDTransactionFactory.cs
@@ -56,30 +56,34 @@ namespace Terradue.Tep {
         }
 
         public List<ASDTransaction> GetServiceTransactionsForASD(int asdid) {
-            List<Transaction> transactions = GetJobTransactionsForASD(asdid);
             var result = new List<ASDTransaction>();
-
-            foreach(var transaction in transactions){
-                var job = WpsJob.FromIdentifier(context, transaction.Identifier);
-                bool exists = false;
-                foreach(var res in result){
-                    if(res.Name == job.WpsName){
-                        res.Balance += transaction.Balance;
-                        if(!res.Owners.Contains(job.Owner.Username)) res.Owners.Add(job.Owner.Username);
-                        exists = true;
-                        continue;
+            try{
+                List<Transaction> transactions = GetJobTransactionsForASD(asdid);
+            
+                foreach(var transaction in transactions){
+                    var job = WpsJob.FromIdentifier(context, transaction.Identifier);
+                    bool exists = false;
+                    foreach(var res in result){
+                        if(res.Name == job.WpsName){
+                            res.Balance += transaction.Balance;
+                            if(!res.Owners.Contains(job.Owner.Username)) res.Owners.Add(job.Owner.Username);
+                            exists = true;
+                            continue;
+                        }
+                    }
+                    if(!exists){
+                        var t = new ASDTransaction()
+                        {
+                            Identifier = job.ProcessId,
+                            Name = job.WpsName,
+                            Balance = transaction.Balance,
+                            Owners = new List<string>{job.Owner.Username}
+                        };
+                        result.Add(t);
                     }
                 }
-                if(!exists){
-                    var t = new ASDTransaction()
-                    {
-                        Identifier = job.ProcessId,
-                        Name = job.WpsName,
-                        Balance = transaction.Balance,
-                        Owners = new List<string>{job.Owner.Username}
-                    };
-                    result.Add(t);
-                }
+            }catch(Exception e){
+                context.LogError(this, e.Message);
             }
             return result;
         }
