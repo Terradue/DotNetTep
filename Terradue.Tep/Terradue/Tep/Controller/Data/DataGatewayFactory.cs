95b8cda452ea44e0a1c75eb3a51ca0a238041e79
eboissier <enguerran.boissier@terradue.com>
2024-04-02 09:50:10 +0200

    up share repo (not catalog.json)

--- Terradue.Tep/Terradue/Tep/Controller/Data/DataGatewayFactory.cs
+++ Terradue.Tep/Terradue/Tep/Controller/Data/DataGatewayFactory.cs
@@ -235,6 +235,9 @@ namespace Terradue.Tep
 
         public static string ShareOnTerrapi(IfyContext context, string workspaceId, string s3link, List<string> users, string publishtoken){
 
+            //we want to share the directory, not the catalog.json
+            s3link = s3link.Substring(0,s3link.LastIndexOf("/"));
+
             var shareInput = new TerrapiShareRequest { 
                 path = s3link,
                 users = users
